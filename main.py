fd0ba58fdccab817f6198c622d727e64959ffe4d
김영빈 <feliciathebeen@gim-yeongbin-ui-MacBookPro.local>
2024-07-04 16:43:01 +0900

    너무 어렵다.

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-# 병아리 팀원들 화이팅
+class Member:
+    def __init__(self, name, username, password):  # 인스턴스의 정의를 해주었다.
+        self.name = name
+        self.username = username
+        self.password = password
+
+    def display(Member):
+        print(f"Name: {Member.name}, Username: {Member.username}")
+
+
+member1 = Member("김영빈", "코딩하는파카", "1234")
+member2 = Member("카트만", "코딩하는카트만", "1234")
+member3 = Member("카일", "코딩하는카일", "1234")
+member1.display()
+member2.display()
+member3.display()
+
+
+class Post:
+    def __init__(self, title, content, author):
+        self.title = title
+        self.content = content
+        self.author = author
+
+    # TODO : 코드 구현이 필요합니다.
+    pass
+
+
+# ----- 코드 실행 ------
+members = []
+posts = []
+
+# TODO : 코드 구현이 필요합니다.
