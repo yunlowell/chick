2b544d024707eac2906e7a4d3293cd1604c191d4
kwang1215 <xman1215@naver.com>
2024-07-04 17:39:07 +0900

    팀 프로젝트 완료.

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ def create_member():
     new_member = Member(name, username, password)
     print('새로운 member가 추가 되었습니다.')
 
+
 class Post:
     def __init__(self, title, content, author):
         self.title = title
@@ -43,13 +44,37 @@ class Post:
 
 
 def create_post():
-    if
-    title = input("작성할 post의 제목을 입력해 주세요.")
-    content = input("작성할 post의 내용을 입력해 주세요.")
-    author = ""
-    new_post = Post(title, content, author)
-    print("")
+    username = write_username()
 
+    for member in members:
+        if username == member.username:
+            while True:
+                password = write_pwd()
+                if password == member.password:
+                    print("로그인 성공")
+                    print(member.username)
+                    title = input("작성할 post의 제목을 입력해 주세요.")
+                    content = input("작성할 post의 내용을 입력해 주세요.")
+                    author = member.username
+                    new_post = Post(title, content, author)
+                    print("post 작성이 완료 되었습니다.")
+                    return
+                else:
+                    print("비밀번호가 일치하지 않습니다. 비밀번호를 재입력하세요.")
+    print("일치하는 ID가 없습니다.")
+
+
+def write_username():
+    input_username = input("ID를 입력하세요.")
+    return input_username
+
+
+def write_pwd():
+    input_pwd = input("비밀번호를 입력하세요.")
+    sha256_hash = hashlib.sha256()
+    sha256_hash.update(input_pwd.encode('UTF-8'))
+    hash_input_pwd = sha256_hash.hexdigest()
+    return hash_input_pwd
 
 
 members = []
@@ -59,7 +84,7 @@ posts = []
 member1 = Member(name="이광열", username="xman1215", password="1234")
 member2 = Member('최준환','asd123',"123456")
 member3 = Member(name="김남훈", username="gkgk55", password="6430")
-member4 = create_member()
+# member4 = create_member()
 
 # Post 클래스 객체 생성
 post1 = Post('제목1','내용1 파이썬',member1.username)
@@ -74,22 +99,44 @@ post7 = Post('제목7','내용7 파이썬',member3.username)
 post8 = Post('제목8','내용8 자바',member3.username)
 post9 = Post('제목9','내용9 html',member3.username)
 
+#
+# # 추가된 회원 정보를 출력
+# for member in members:
+#     member.display()
+#
+# # 해싱된 회원들 비밀번호 출력
+# for member in members:
+#     member.show_password()
+#
+# # 특정 유저가 작성한 게시글의 제목 출력
+# for member in members:
+#     for post in posts:
+#         if post.author == member.username:
+#             print(f'{member.username}님이 작성한 게시글의 제목: {post.title}')
+#
+# # 특정 단어가 content에 포함된 게시글의 제목을 출력
+# for post in posts:
+#     if "파이썬" in post.content :
+#         print(f'파이썬이 포함된 게시글의 제목: {post.title}')
 
-# 추가된 회원 정보를 출력
+
+while True:
+    choice = input("member 생성은 1번 / post 작성은 2번 / 종료는 3번을 눌러주세요.")
+    if choice == '1':
+        create_member()
+    elif choice == '2':
+        create_post()
+    elif choice == '3':
+        break
+    else:
+        print("1,2,3중 하나만 입력하세요.")
+print("")
+
+print("member 리스트")
 for member in members:
     member.display()
+print("")
 
-# 해싱된 회원들 비밀번호 출력
-for member in members:
-    member.show_password()
-
-# 특정 유저가 작성한 게시글의 제목 출력
+print("post 리스트")
 for post in posts:
-    if post.author == member1.username:
-        print(f'{member1.username}님이 작성한 게시글의 제목: {post.title}')
-
-# 특정 단어가 content에 포함된 게시글의 제목을 출력
-for post in posts:
-    if "파이썬" in post.content :
-        print(f'파이썬이 포함된 게시글의 제목: {post.title}')
-
+    post.display()
