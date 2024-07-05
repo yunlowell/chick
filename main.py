5a8415b21652bf5b1674d3882f6e8585afe2699e
feliciathebeen <feliciathebeen@gmail.com>
2024-07-05 10:21:37 +0900

    기본 과제내용 완료

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
-# testdsdsdsd
 class Member:
-    def __init__(self, name, username, password):  # 인스턴스의 정의를 해주었다.
+    def __init__(self, name, username, password):
         self.name = name
         self.username = username
         self.password = password
 
-    def display(Member):
-        print(f"Name: {Member.name}, Username: {Member.username}")
-
-
-member1 = Member("김영빈", "코딩하는파카", "1234")
-member2 = Member("카트만", "코딩하는카트만", "1234")
-member3 = Member("카일", "코딩하는카일", "1234")
-member1.display()
-member2.display()
-member3.display()
-
+    def display(self):
+        print(f"Name: {self.name}, Username: {self.username}")
 
 class Post:
     def __init__(self, title, content, author):
@@ -23,12 +13,35 @@ class Post:
         self.content = content
         self.author = author
 
-    # TODO : 코드 구현이 필요합니다.
-    pass
-
-
-# ----- 코드 실행 ------
 members = []
-posts = []
+members.append(Member("파카", "코딩하는파카", "1234"))
+members.append(Member("카트만", "코딩하는카트만", "1234"))
+members.append(Member("카일", "코딩하는카일", "1234"))
 
-# TODO : 코드 구현이 필요합니다.
+for member in members:
+    member.display()
+
+posts = []
+posts.append(Post("제목 파카1", "파카1의 post", "파카"))
+posts.append(Post("제목 파카2", "파카2의 post", "파카"))
+posts.append(Post("제목 파카3", "파카3의 post", "파카"))
+posts.append(Post("제목 카트만1", "카트만1의 post", "카트만"))
+posts.append(Post("제목 카트만2", "카트만2의 post", "카트만"))
+posts.append(Post("제목 카트만3", "카트만3의 post", "카트만"))
+posts.append(Post("제목 카일1", "카일1의 post", "카일"))
+posts.append(Post("제목 카일2", "카일2의 post", "카일"))
+posts.append(Post("제목 카일3", "카일3의 post", "카일"))
+
+for post in posts:
+    if post.author == "파카":
+        print(post.title)
+        continue
+    else:
+        print()
+        break
+
+word = "카트만"
+for post in posts:
+    if word in post.content:
+        print(post.title)
+
