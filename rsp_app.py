e45ca44b9da439feda9eecedbd339e7c7e52e396
kwang1215 <xman1215@naver.com>
2024-07-10 10:30:14 +0900

    rsp_game app.py update

--- rsp_app.py
+++ rsp_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session
 import random
 import os
 from flask_sqlalchemy import SQLAlchemy
@@ -8,6 +8,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'your_secret_key'  # 세션 사용을 위한 비밀 키 설정
 
 db = SQLAlchemy(app)
 
@@ -34,24 +35,25 @@ with app.app_context():
 
 @app.route('/')
 def home():
-    return render_template('rsp_game.html')
+    return render_template('rsp_game.html', data=None)
 
 
 @app.route('/play', methods=['POST'])
 def play():
     options = ['가위', '바위', '보']
-    player_choice = request.form['choice']
+
+    player_choice = request.form.get('choice')
     computer_choice = random.choice(options)
 
     # 유저 게임 룰
     if player_choice == computer_choice:
-        result = 'draw'
+        result = '무승부입니다.'
     elif (player_choice == '가위' and computer_choice == '보') or \
             (player_choice == '바위' and computer_choice == '가위') or \
             (player_choice == '보' and computer_choice == '바위'):
-        result = 'win'
+        result = '이겼습니다.'
     else:
-        result = 'lose'
+        result = '졌습니다.'
 
     # 유저 게임 내용 db에 저장
     game_result = GameResult(player_choice=player_choice, computer_choice=computer_choice,
@@ -65,25 +67,55 @@ def play():
         user_game_score = UserGameScore(win=0, draw=0, loss=0)
         db.session.add(user_game_score)
 
-    if result == 'win':
+    if result == '이겼습니다.':
         user_game_score.win += 1
-    elif result == 'draw':
+    elif result == '무승부입니다.':
         user_game_score.draw += 1
     else:
         user_game_score.loss += 1
 
     db.session.commit()
 
+    # 세션에 최신 게임 결과 저장
+    session['player_choice'] = player_choice
+    session['computer_choice'] = computer_choice
+    session['result'] = result
+
+    # PRG 패턴 적용: 리디렉션
+    return redirect(url_for('show_result'))
+
+
+@app.route('/result')
+def show_result():
+    # 세션에서 플레이 결과 불러오기
+    player_choice = session.pop('player_choice', None)
+    computer_choice = session.pop('computer_choice', None)
+    result = session.pop('result', None)
+
+    # 최신 게임 결과 가져오기
+    latest_game_result = GameResult.query.order_by(GameResult.id.desc()).first()
+    user_game_score = UserGameScore.query.first()
+
+    game_result_list = GameResult.query.all()
+
     context = {
-        'player_choice': player_choice,
-        'computer_choice': computer_choice,
-        'result': result,
+        'player_choice': latest_game_result.player_choice,
+        'computer_choice': latest_game_result.computer_choice,
+        'result': latest_game_result.result,
         'win': user_game_score.win,
         'draw': user_game_score.draw,
-        'loss': user_game_score.loss
+        'loss': user_game_score.loss,
+        'game_result_list': game_result_list
     }
 
     return render_template('rsp_game.html', data=context)
+
+
+@app.route('/delete', methods=['POST'])
+def delete():
+    db.drop_all()
+    db.create_all()
+    return redirect(url_for('home'))
 
 
 if __name__ == '__main__':
